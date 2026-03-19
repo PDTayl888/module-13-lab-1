044969b4c5eda49fe119dfdeb8f37be4d44885f1
Patrick D. Taylor <patrickdtaylor1980@gmail.com>
2026-03-18 23:30:42 -0400

    edited server

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
 const express = require('express');
+const { MongoClient } = require('mongodb');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const PORT = 3001;
+
+const uri = process.env.MONGO_URI;
+
